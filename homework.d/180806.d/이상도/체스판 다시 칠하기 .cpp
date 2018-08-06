8cd813988f9a453ab1cb1b5bbff8ad4dd1a51aa2
SD-PC\SD <sangdo913@naver.com>
2018-08-06 20:31:45 +0900

    숙제 제출

---
+++ homework.d/180806.d/이상도/체스판 다시 칠하기 .cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+
+using namespace std;
+
+char PaintChess_map[50][50];
+
+inline int PaintChess_min(int i1, int i2)
+{
+	return i1 < i2 ? i1 : i2;
+}
+
+int makeChess(int r, int c)
+{
+	bool check = true;
+	int repair = 0;
+	for (int i = r; i < r + 8; i++)
+	{
+		for (int j = c; j < c + 8; j++)
+		{
+			repair = check ^ (PaintChess_map[i][j] == 'W') ? ++repair : repair;
+			check = !check;
+		}
+		check = !check;
+	}
+	return PaintChess_min(repair, 64 - repair);
+}
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	for (int r = 0; r < n; r++)
+	{
+		for (int c = 0; c < m; c++)
+		{
+			cin >> PaintChess_map[r][c];
+		}
+	}
+	int res = 65;
+	for (int r = 0; r <= n - 8; r++)
+	{
+		for (int c = 0; c <= m - 8; c++)
+		{
+			res = PaintChess_min(res, makeChess(r, c));
+		}
+	}
+	cout << res << '\n';
+	return 0;
+}
